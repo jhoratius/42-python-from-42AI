23a3bd55bbb348bb5e936744156f500c025e4e3a
jhoratiu <jhoratiu@f2r6s10.paris.42.school>
2026-07-19 19:39:25 +0200

    done with ex02 module03

--- module_03/ex02/ScrapBooker.py
+++ module_03/ex02/ScrapBooker.py
@@ -2,87 +2,138 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class ScrapBooker:
-	def crop(self, array, dim, position=(0,0)):
+    def crop(self, array, dim, position=(0,0)):
 
-		x_max, y_max = array.shape()
-		if dim[0] > x_max:
-			assert 
+        x_max, y_max = array.shape
+        try:
+            if dim[0] < 0 or dim[0] > x_max:
+                raise ValueError("over the boundaries")
+            if dim[1] < 0 or dim[1] > y_max:
+                raise ValueError("over the boundaries")
+            if position[0] < 0 or position[0] >= x_max:
+                raise ValueError("over the boundaries")
+            if position[1] < 0 or position[1] >= y_max:
+                raise ValueError("over the boundaries")
 
-		new_arr = array[position[0]:position[0] + dim[0], position[1]: position[1] + dim[1]]
-		return new_arr
+            if position[0] + dim[0] > x_max or position[1] + dim[1] > y_max:
+                raise ValueError("over the boundaries")
 
-	# Crops the image as a rectangle via dim arguments (being the new height
-	# and width of the image) from the coordinates given by position arguments.
+            new_arr = array[position[0]:position[0] + dim[0], position[1]: position[1] + dim[1]]
+            return new_arr
+        except Exception as e:
+            print(f"Error: {e}")
+    # Crops the image as a rectangle via dim arguments (being the new height
+    # and width of the image) from the coordinates given by position arguments.
 
-	# Args:
-	# -----
-	# array: numpy.ndarray
-	# dim: tuple of 2 integers.
-	# position: tuple of 2 integers.
+    # Args:
+    # -----
+    # array: numpy.ndarray
+    # dim: tuple of 2 integers.
+    # position: tuple of 2 integers.
 
-	# Returns:
-	# -------
-	# new_arr: the cropped numpy.ndarray.
-	# None: (if the combination of parameters is not possible).
+    # Returns:
+    # -------
+    # new_arr: the cropped numpy.ndarray.
+    # None: (if the combination of parameters is not possible).
 
-	# Raises:
-	# ------
-	# This function should not raise any Exception.
+    # Raises:
+    # ------
+    # This function should not raise any Exception.
 
-	# def thin(self, array, n, axis):
+    def thin(self, array, n, axis):
 
-	# Deletes every n-th line pixels along the specified axis (0: vertical, 1: horizontal)
+        x_max, y_max = array.shape
+        try:
+            if n > x_max or n > y_max:
+                raise ValueError("Over the boundaries.")
+            if axis < 0 or axis > 1:
+                raise ValueError("Wrong axis.")
 
-	# Args:
-	# -----
-	# array: numpy.ndarray
-	# n: non null positive integer lower than the number of row/column of the array
-	# (depending of axis value).
-	# axis: positive non null integer.
+            # i = 0
+            # new_arr = array
+            # while(i < n):
+            #     new_arr = np.delete(array, i, axis)
+            #     i += 1
+            new_arr = np.delete(array, np.s_[0:n], axis)
+            return (new_arr)
 
-	# Returns:
-	# -------
-	# new_arr: thined numpy.ndarray.
-	# None: (if the combination of parameters is not possible).
+        except Exception as e:
+            print(f"Error: {e}")
 
-	# Raises:
-	# ------
-	# This function should not raise any Exception.
+    # Deletes every n-th line pixels along the specified axis (0: vertical, 1: horizontal)
 
-	# def juxtapose(self, array, n, axis):
+    # Args:
+    # -----
+    # array: numpy.ndarray
+    # n: non null positive integer lower than the number of row/column of the array
+    # (depending of axis value).
+    # axis: positive non null integer.
 
-	# Juxtaposes n copies of the image along the specified axis.
+    # Returns:
+    # -------
+    # new_arr: thined numpy.ndarray.
+    # None: (if the combination of parameters is not possible).
 
-	# Args:
-	# -----
-	# array: axis:numpy.ndarray.
-	# n: positive non null integer.
-	# axis: integer of value 0 or 1.
+    # Raises:
+    # ------
+    # This function should not raise any Exception.
 
-	# Returns:
-	# -------
-	# new_arr: juxtaposed numpy.ndarray.
-	# None: (if the combination of parameters is not possible).
+    def juxtapose(self, array, n, axis):
+        reps = [1] * array.ndim
+        reps[axis] = n
+        return np.tile(array, reps)
 
-	# Raises:
-	# -------
-	# This function should not raise any Exception.
+    # Imagine you have a grayscale image (array.ndim = 2). You want to tile it horizontally (axis = 1) and make n = 3 copies.
+    # Line 1: reps = [1] * array.ndim
+    # Since ndim is 2, this creates:
+    # reps = [1, 1] (Default: Don't repeat rows, don't repeat columns)
+    # Line 2: reps[axis] = n
+    # Since axis = 1 and n = 3, this changes the value at index 1:
+    # reps[1] = 3
+    # Now, reps becomes: [1, 3]
+    # When you pass [1, 3] to np.tile(), it looks at the instructions: "Repeat rows 1 time (no change), repeat columns 3 times." You get a perfect horizontal strip of 3 images.
 
-	# def mosaic(self, array, dim):
+    # Juxtaposes n copies of the image along the specified axis.
 
-	# Makes a grid with multiple copies of the array. The dim argument specifies
-	# the number of repetition along each dimensions.
+    # Args:
+    # -----
+    # array: axis:numpy.ndarray.
+    # n: positive non null integer.
+    # axis: integer of value 0 or 1.
 
-	# Args:
-	# -----
-	# array: tuple of 2 integers.
-	# dim: numpy.ndarray.
+    # Returns:
+    # -------
+    # new_arr: juxtaposed numpy.ndarray.
+    # None: (if the combination of parameters is not possible).
 
-	# Return:
-	# -----
-	# new_arr: mosaic numpy.ndarray.
-	# None (combinaison of parameters not compatible).
+    # Raises:
+    # -------
+    # This function should not raise any Exception.
 
-	# Raises:
-	# -------
-	# This function should not raise any Exception.
+    def mosaic(self, array, dim):
+
+        new_arr = self.juxtapose(array, dim[0], 0)
+        new_arr = self.juxtapose(new_arr, dim[1], 1)
+        return new_arr
+        # reps = [1] * dim
+        # reps[0] = dim
+        # new_arr = np.tile(array, reps)
+        # reps2 = [1] * dim
+        # reps2[1] = dim
+        # new_arr = np.tile(array, reps2)
+
+    # Makes a grid with multiple copies of the array. The dim argument specifies the number of repetition along each dimensions.
+
+    # Args:
+    # -----
+    # array: tuple of 2 integers.
+    # dim: numpy.ndarray.
+
+    # Return:
+    # -----
+    # new_arr: mosaic numpy.ndarray.
+    # None (combinaison of parameters not compatible).
+
+    # Raises:
+    # -------
+    # This function should not raise any Exception.
--- module_03/ex02/tests.py
+++ module_03/ex02/tests.py
@@ -2,12 +2,23 @@ import numpy as np
 from ScrapBooker import ScrapBooker
 
 spb = ScrapBooker()
-arr1 = np.arange(0,25).reshape(5,5)
-print(arr1)
-print(spb.crop(arr1, (3,1),(1,0)))
+# arr1 = np.arange(0,25).reshape(5,5)
+# print(arr1)
+# print(spb.crop(arr1, (3,1),(1,0)))
 
 # arr2 = np.array("A B C D E F G H I".split() * 6).reshape(-1,9)
-# spb.thin(arr2,3,0)
+# print(arr2)
+# print("")
+# print(spb.thin(arr2,3,0))
+# print("")
 
 # arr3 = np.array([[1, 2, 3],[1, 2, 3],[1, 2, 3]])
-# spb.juxtapose(arr3, 3, 1)
+# print(arr3)
+# print("")
+# print(spb.juxtapose(arr3, 3, 1))
+# print("")
+
+arr4 = np.array([[1, 2, 3],[1, 2, 3],[1, 2, 3]])
+print(arr4)
+print("")
+print(spb.mosaic(arr4, (3, 3)))
