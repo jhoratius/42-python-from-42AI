43bdebcd8be44f9c11a85f39f0cd7c64a1afcb3f
Junichi Junichi <julien.horatius@gmail.com>
2026-07-23 13:10:24 +0200

    progress on ex00 md05

--- 2-ml-piscine/module_05/ex00/matrix.py
+++ 2-ml-piscine/module_05/ex00/matrix.py
@@ -1,44 +1,99 @@
 import numpy as np
 
 class Matrix:
-    def __init__(self, data: list[list[float]] = [[1.0, 2.0], [3.0, 4.0]], shape: tuple[int, int] = (0, 0)):
-        self.data = data
-        self.shape = shape
+
+    def __init__(self, arg):
+        if isinstance(arg, list):
+            self.data = arg
+            rows = len(arg)
+            cols = len(arg[0])
+            self.shape = (rows, cols)
+
+        elif isinstance(arg, tuple):
+            print()
+            self.data = [[[0] * arg[0]] * arg[1]]
+            self.shape = arg
+
+        else:
+            raise TypeError("Data must be a list of lists or a shape tuple.")
 
     def __str__(self):
-        """Matrix class"""
-        return "Matrix class"
-    
+        return str(self.data)
+
     # def __repr__(self):
+    
+    def create_matrix(shape: tuple) -> list[list[float]]:
+        rows, cols = shape
+        res = [[0.0 for _ in range(cols)] for _ in range(rows)]
+        return res
 
     # add : only matrices of same dimensions.
-    def __add__(self, Matrix):
-        # make the addition of two matrices
-        return self.Matrix.add(Matrix)
+    def __add__(self, other):
+        
+        mat1 = self.data
+        mat2 = other.data
+        mat_res = [[0.0 for _ in range(mat1.shape[1])] for _ in range(mat2.shape[0])]
+        for val1 in mat1:
+            for val2 in mat2:
+                mat_res[i][j] = val1 + val2
+        # mat_res = [val1 + val2 for val1 in mat1, for val2 in mat2]
+        return mat_res
 
-    def __radd__(self, Matrix):
-        # sub : only matrices of same dimensions.
-        return Matrix.add(self.Matrix)
+    # def __radd__(self, other):
+    #     mat1 = other.data
+    #     mat2 = self.data
+    #     mat_res = [val1 + val2 for val1 in mat1, for val2 in mat2]
+    #     return mat_res
 
-    def __sub__(self, Matrix):
-        return self.Matrix.sub(Matrix)
+    # def __sub__(self, other):
+    #     mat1 = self.data
+    #     mat2 = other.data
+    #     mat_res = [val1 - val2 for val1 in mat1, for val2 in mat2]
+    #     return mat_res
 
-    def __rsub__(self, Matrix):
-        # div : only scalars.
-        return Matrix.sub(Matrix, self.Matrix)
+    # def __rsub__(self, other):
+    #     mat1 = other.data
+    #     mat2 = self.data
+    #     mat_res = [val1 - val2 for val1 in mat1, for val2 in mat2]
+    #     return mat_res
 
-    def __truediv__(self, Matrix):
-        return np.__truediv__(self.Matrix, Matrix)
+    # def __truediv__(self, other):
+    #     mat1 = other.data
+    #     mat2 = self.data
+    #     mat_res = [val1 / val2 for val1 in mat1, for val2 in mat2]
+    #     return mat_res
 
-    def __rtruediv__(self, Matrix):
-        # mul : scalars, vectors and matrices , can have errors with vectors and matrices,
-        # returns a Vector if we perform Matrix * Vector mutliplication.
-        return np.__truediv__(Matrix, self.Matrix)
+    # def __rtruediv__(self, other):
+    #     mat1 = self.data
+    #     mat2 = other.data
+    #     mat_res = [val1 / val2 for val1 in mat1, for val2 in mat2]
+    #     return mat_res
+    #     # mul : scalars, vectors and matrices , can have errors with vectors and matrices,
+    #     # returns a Vector if we perform Matrix * Vector mutliplication.
+    #     return np.__truediv__(other.data, self.data)
+    # MN = (M[0][0] * N[0][0] + M[0][1] * N[1][0], M[0][0] * N[0][1] + M[0][1] * N[1][1])
+        #  (M[0][0] * N[0][0] + M[0][1] * N[1][0], M[0][0] * N[0][1] + M[0][1] * N[1][1])
 
-    def __mul__(self, Matrix):
-        return self.Matrix * Matrix
+    # def __mul__(self, other):
+    #     rows_A = len(self.data)
+    #     cols_A = len(self.data[0])
+    #     rows_B = len(other.data)
+    #     cols_B = len(other.data[0])
 
-    def __rmul__(self, Matrix):
-        return Matrix * self.Matrix
+    #     if cols_A != rows_B:
+    #         raise ValueError(f"Cannot multiply matrices of shape ({rows_A}, {cols_A}) and ({rows_B}, {cols_B})")
+        
+    #     result = self.create_matrix((cols_B, rows_A))
+
+    #     # 2 rows
+    #     for i in range(rows_A):
+    #         # 4 columns
+    #         for j in range(cols_B):
+    #             print()
+
+    #     return result
+
+    # def __rmul__(self, other):
+    #     return np.dot(other.data, self.data)
 
     # def T():
--- 2-ml-piscine/module_05/ex00/test.py
+++ 2-ml-piscine/module_05/ex00/test.py
@@ -5,52 +5,74 @@ m1.shape
 # Output:
 # (3, 2)
 
-m1.T()
-# Output:
-# Matrix([[0., 2., 4.], [1., 3., 5.]])
+# m1.T()
+# # Output:
+# # Matrix([[0., 2., 4.], [1., 3., 5.]])
 
-m1.T().shape
-# Output:
-# (2, 3)
+# m1.T().shape
+# # Output:
+# # (2, 3)
 
-m1 = Matrix([[0., 2., 4.], [1., 3., 5.]])
-m1.shape
-# Output:
-# (2, 3)
+# m1 = Matrix([[0., 2., 4.], [1., 3., 5.]])
+# m1.shape
+# # Output:
+# # (2, 3)
 
-m1.T()
-# Output:
-# Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+# m1.T()
+# # Output:
+# # Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
 
-m1.T().shape
-# Output:
-# (3, 2)
+# m1.T().shape
+# # Output:
+# # (3, 2)
 
-m1 = Matrix([[0.0, 1.0, 2.0, 3.0],
-[0.0, 2.0, 4.0, 6.0]])
-m2 = Matrix([[0.0, 1.0],
-[2.0, 3.0],
-[4.0, 5.0],
-[6.0, 7.0]])
-m1 * m2
-# Output:
-# Matrix([[28., 34.], [56., 68.]])
+# m1 = Matrix([[0.0, 1.0, 2.0, 3.0],
+#              [0.0, 2.0, 4.0, 6.0]])
+# m2 = Matrix([[0.0, 1.0],
+#              [2.0, 3.0],
+#              [4.0, 5.0],
+#              [6.0, 7.0]])
 
-m1 = Matrix([[0.0, 1.0, 2.0],
-[0.0, 2.0, 4.0]])
-v1 = Vector([[1], [2], [3]])
-m1 * v1
-# Output:
-# Matrix([[8], [16]])
+m1 = Matrix([[0.0, 1.0, 2.0, 3.0], [0.0, 2.0, 4.0, 6.0]])
+
+m2 = Matrix([
+    [0.0, 1.0],
+    [2.0, 3.0],
+    [4.0, 5.0],
+    [6.0, 7.0]
+])
+
+print(f"add: {m1 + m2}")
+print(f"radd: {m2 + m1}")
+print(f"sub: {m1 - m2}")
+print(f"rsub: {m2 - m1}")
+print(f"truediv: {m1 / m2}")
+print(f"rtruediv: {m2 / m1}")
+
+
+
+m3 = Matrix((2,2))
+print(m3)
+
+# print(m1 * m2)
+# # Output:
+# # Matrix([[28., 34.], [56., 68.]])
+
+# m1 = Matrix([[0.0, 1.0, 2.0],
+# [0.0, 2.0, 4.0]])
+# v1 = Vector([[1], [2], [3]])
+# m1 * v1
+# # Output:
+# # Matrix([[8], [16]])
 
 # Or: Vector([[8], [16]
-v1 = Vector([[1], [2], [3]])
-v2 = Vector([[2], [4], [8]])
-v1 + v2
-# Output:
-# Vector([[3],[6],[11]])
+# v1 = Vector([[1], [2], [3]])
+# v2 = Vector([[2], [4], [8]])
+# v1 + v2
+# # Output:
+# # Vector([[3],[6],[11]])
 
 
-v1 = Vector([[1, 2, 3]]) # create a row vector
-v2 = Vector([[1], [2], [3]]) # create a column vector
-v3 = Vector([[1, 2], [3, 4]]) # return an error
+# v1 = Vector([[1, 2, 3]]) # create a row vector
+# v2 = Vector([[1], [2], [3]]) # create a column vector
+# v3 = Vector([[1, 2], [3, 4]]) # return an error
